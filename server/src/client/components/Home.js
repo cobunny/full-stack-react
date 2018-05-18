8f3fed534a311790074973b8a3121454a8d256b6
Cobunny <malatanggm@gmail.com>
2018-05-18 14:22:12 -0700

    Generated bundle.js for client

--- server/src/client/components/Home.js
+++ server/src/client/components/Home.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Home = () => {
-  return <div>Home component</div>;
+  return <h1>Home component</h1>;
 };
 
 export default Home;
--- server/src/index.js
+++ server/src/index.js
@@ -6,10 +6,26 @@ import Home from './client/components/Home';
 
 const app = express();
 
+app.use(express.static('public'));
+
 app.get('/', (req, res) => {
   const content = renderToString(<Home />);
 
-  res.send(content);
+  const html = `<!DOCTYPE html>
+  <html lang="en">
+  <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <meta http-equiv="X-UA-Compatible" content="ie=edge">
+      <title>Full Stack React Sample</title>
+  </head>
+  <body>
+      <div id="root">${content}</div>
+      <script src="bundle.js"></script>
+  </body>
+  </html>`;
+
+  res.send(html);
 });
 
 app.listen(PORT, function listenHandler() {
