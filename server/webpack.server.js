9b0f93e71bfade8dbe95888eef7302f654cd90ff
Cobunny <malatanggm@gmail.com>
2018-05-18 16:00:03 -0700

    Add npm dev start and reduce file size of server-side bundle.js

--- server/webpack.server.js
+++ server/webpack.server.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const merge = require('webpack-merge');
 const baseConfig = require('./webpack.base.js');
+const webpackNodeExternals = require('webpack-node-externals');
 
 const config = {
   target: 'node',
@@ -8,7 +9,9 @@ const config = {
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'build')
-  }
+  },
+  // Ignore node_modules when bundling in Webpack on the server.
+  externals: [webpackNodeExternals()]
 };
 
 module.exports = merge(baseConfig, config);
