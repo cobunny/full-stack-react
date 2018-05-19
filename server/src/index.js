f2c77e136c83ff404baf6caf63589200873f409b
Cobunny <malatanggm@gmail.com>
2018-05-18 17:18:34 -0700

    Move base html template string from app root route handler to a separate file

--- server/src/index.js
+++ server/src/index.js
@@ -1,31 +1,13 @@
 import express from 'express';
-import React from 'react';
-import { renderToString } from 'react-dom/server';
+import renderer from './helpers/renderer';
 import PORT from './config';
-import Home from './client/components/Home';
 
 const app = express();
 
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-  const content = renderToString(<Home />);
-
-  const html = `<!DOCTYPE html>
-  <html lang="en">
-  <head>
-      <meta charset="UTF-8">
-      <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      <meta http-equiv="X-UA-Compatible" content="ie=edge">
-      <title>Full Stack React Sample</title>
-  </head>
-  <body>
-      <div id="root">${content}</div>
-      <script src="bundle.js"></script>
-  </body>
-  </html>`;
-
-  res.send(html);
+  res.send(renderer());
 });
 
 app.listen(PORT, function listenHandler() {
