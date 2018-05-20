a142e5265dc62d0bdc60a0161e0b770ac6013668
Cobunny <malatanggm@gmail.com>
2018-05-19 23:25:58 -0700

    Added redux stores and usersReducer

--- server/src/client/client.js
+++ server/src/client/client.js
@@ -2,11 +2,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
 import Routes from './Routes';
+import reducers from './reducers';
+
+// Client-side redux store.
+const store = createStore(reducers, {}, applyMiddleware(thunk));
 
 ReactDOM.hydrate(
-  <BrowserRouter>
-    <Routes />
-  </BrowserRouter>,
+  <Provider store={store}>
+    <BrowserRouter>
+      <Routes />
+    </BrowserRouter>
+  </Provider>,
   document.querySelector('#root')
 );
--- server/src/index.js
+++ server/src/index.js
@@ -1,13 +1,16 @@
 import express from 'express';
 import renderer from './helpers/renderer';
 import PORT from './config';
+import createStore from './helpers/createStore';
 
 const app = express();
 
 app.use(express.static('public'));
 
 app.get('*', (req, res) => {
-  res.send(renderer(req));
+  const store = createStore();
+
+  res.send(renderer(req, store));
 });
 
 app.listen(PORT, function listenHandler() {
