542366373eaab2cdabfe67bb5b112e4ecce4b330
Cobunny <malatanggm@gmail.com>
2018-05-21 01:47:37 -0700

    Add 404 response status

--- server/src/index.js
+++ server/src/index.js
@@ -31,7 +31,12 @@ app.get('*', (req, res) => {
 
   // Render the app after all the promises are resolved
   Promise.all(promises).then(() => {
-    res.send(renderer(req, store));
+    const context = {};
+    const content = renderer(req, store, context);
+    if (context.notFound) {
+      res.status(404);
+    }
+    res.send(content);
   });
 });
 
